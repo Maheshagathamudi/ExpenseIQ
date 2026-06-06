b32fd59d7bc09945d6a99a52ad5420b9f124321e
Agathamudi Mahesh <111571121+Maheshagathamudi@users.noreply.github.com>
2026-06-06 12:53:08 +0530

    Update API base URL and refactor request interceptor

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,17 +1,23 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expenseiq-dbtv.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
-});
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
